c9ff0478c824c7a69fb3929458228353cd95bec9
uchiyama00 <nowall@nowall-no-MacBook-Air-2.local>
2016-11-14 10:32:38 +0900

    add
    他クラスのプロパティやメソッドを使いまわした。

--- note9-2-1/ViewController.swift
+++ note9-2-1/ViewController.swift
@@ -10,21 +10,29 @@ import UIKit
 
 
 
-class MyClass{
-    let msg = "ハロー"
-    //MyClassというクラス
+class MyClass {
+    //let msg = "ハロー"
+    var msg = "ハロー"
+    //MyClassというクラス。→下で違うクラスの中でmsgを使い回すために変数に変更した。
     func hello() {
         //とmsgというインスタンスプロパティと、helloというインスタンスメソッドを定義した
         print(msg)
     }
-}
+    
+}//ここまででMyClass
 
 class ViewController: UIViewController {
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let myObj = MyClass()//MyClassのインスタンスを作って変数myObj入れた。
+        
+        myObj.hello()//hello()を実行した。
+        //MyObjというMyClassのインスタンスプロパティにドットシンタックスでアクセスしている。
+        //つまり.helloはmyObjにアクセスしている。
+        myObj.msg = "はーい、元気？"
+        //.msgは上で定義したmyObjにアクセスしている。
+        myObj.hello()//９−２−１ではこのクラスは空だったのでMyclassのmsgは定数だったが、今回はそれを使いまわし、値を変えるのでコメントアウトし、変数に設定した。
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
---
+++ note9-2-1/swiftNewFile.swift
@@ -0,0 +1,22 @@
+//
+//  swiftNewFile.swift
+//  note9-2-1
+//
+//  Created by nowall on 2016/11/14.
+//  Copyright © 2016年 nakagami. All rights reserved.
+//
+
+import Foundation
+
+class MyClass {
+    
+    let msg = "ハロー"
+    
+    func hello() {
+        print(msg)
+    }
+    
+    
+    
+    
+}
