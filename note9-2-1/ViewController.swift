cccced1e82a1b4f0f5a980aa61a1ed77a4adbd6a
uchiyama00 <nowall@nowall-no-MacBook-Air-2.local>
2016-11-14 09:54:10 +0900

    add
    スパルタキャンプで理解できなかったプロパティとメソッドという言葉が何を指しているのかを確認した

--- note9-2-1/ViewController.swift
+++ note9-2-1/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class MyClass{
+    let msg = "ハロー"
+    //MyClassというクラス
+    func hello() {
+        //とmsgというインスタンスプロパティと、helloというインスタンスメソッドを定義した
+        print(msg)
+    }
+}
+
+class ViewController: UIViewController {
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
